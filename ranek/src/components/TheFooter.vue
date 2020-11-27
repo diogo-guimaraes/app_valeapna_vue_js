f4477199f650e11a20efb9ef867bc94a402e0328
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-11-27 11:45:14 -0300

    add(doc): Projeto inicial app valeapna

---
+++ ranek/src/components/TheFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- refactor(compoments): TheFooter
+add(compoments): TheFooter
+  rm(compoments): TheFooter-->
+  <footer>
+    <p>© Copyright 2020-2020 PnA Publicidade Ltda.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+};
+</script>
+
+<style scoped>
+footer {
+  background: #252525;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #fff;
+  position: relative;
+  top: 75px;
+}
+</style>
---
+++ ranek/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 
+    refactor(compoments): Home
+    add(compoments): Home
+    rm(compoments): Home
+  -->
+  <section>
+    <h1>PnA Webservice Aplicações</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
+</template>
+
+<script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
+export default {
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: #252525;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
